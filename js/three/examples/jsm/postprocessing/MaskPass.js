8f78277a14ae66ac4c0a619beb5ef949f89e10cf
Zao <zawonyee@163.com>
2026-01-02 02:09:57 +0800

    Add missing MaskPass.js

---
+++ js/three/examples/jsm/postprocessing/MaskPass.js
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error>
+  <Code>NoSuchKey</Code>
+  <Message>The specified key does not exist.</Message>
+  <RequestId>6956B7836BE0883630B6FE99</RequestId>
+  <HostId>fe-static-zbprod-zb1-oss-3.oss-cn-zhangjiakou.aliyuncs.com</HostId>
+  <Key>three@0.160.0/examples/jsm/postprocessing/MaskPass.js</Key>
+  <EC>0026-00000001</EC>
+  <RecommendDoc>https://api.aliyun.com/troubleshoot?q=0026-00000001</RecommendDoc>
+</Error>
